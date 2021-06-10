8b3a597bd6649bb2a5e0fbd6c3b6c02c04d4c010
Allysa Joy Murillo <82067938+allysajoymurillo@users.noreply.github.com>
2021-06-10 16:37:09 +0800

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ProductComplaints
+{
+
+class Product
+{
+    public Product()
+    {
+        Console.WriteLine("Product Complaints!");
+    }
+    public void Name()
+    {
+        Console.WriteLine("Product Name: Victoria's Secret Perfume");
+    }
+    public void Scent()
+    {
+        Console.WriteLine("Product Perfume scent: Love Spell");
+    }
+    public void Code()
+    {
+        Console.WriteLine("Product Perfume Code: 0667538086212");
+    }
+        public virtual void Mass()
+    {
+        Console.WriteLine("Product Mass: 250 ml");
+    }
+}
+      
+      
+     
